2693f51bef0436dc83cb40249651039d3a36b50e
ridnex <ridnex28@gmail.com>
2022-03-15 09:05:37 +0600

    lab 6

--- lab 6/dir&file/8.py
+++ lab 6/dir&file/8.py
@@ -1,11 +1,10 @@
 #Write a Python program to delete file by specified path. Before deleting check for access and whether a given path exists or not.
 
 import os
-import shutil
-#i will delete file "deleteme_copy.txt" which from 6.py
-shutil.copy("deleteme.txt", "deleteme_copy.txt")
-if os.path.exists("deleteme_copy.txt"):
-    os.remove("deleteme_copy.txt")
+paths = os.getcwd()  
+path_file = os.path.join(paths, "deleteme.txt" )
+if os.path.exists(path_file):
+    os.remove(os.path.basename(path_file))
     print("deleted")
 else:
     print("no such file")
