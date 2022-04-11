13ec34f4f9be0a97e0e3fee8c95a8637eff9b6d7
ridnex <ridnex28@gmail.com>
2022-04-11 13:59:37 +0600

    Racer

--- lab 8/Racer.py
+++ lab 8/Racer.py
@@ -1,5 +1,3 @@
-from email.headerregistry import ContentDispositionHeader
-from turtle import screensize
 import pygame
 import random, time
  
@@ -53,7 +51,7 @@ class Coin(pygame.sprite.Sprite):
     def change_location(self):
         self.rect.center = (random.randint(40, w-40), 0 )
     def move(self):    
-        self.rect.move_ip(0,5)
+        self.rect.move_ip(0,3)
         if (self.rect.top > 600):
             self.rect.top = 0
             self.rect.center = (random.randint(40, w - 40), 0)
@@ -74,7 +72,7 @@ class Enemy(pygame.sprite.Sprite):
             SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(40, w - 40), 0)
-        if SCORE %5 == 0 and SCORE != 0:
+        if SCORE %10 == 0 and SCORE != 0:
             SPEED += 0.5
  
  
@@ -86,7 +84,6 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (160, 520)
         
     def move(self):
-        global SPEED
         pressed_keys = pygame.key.get_pressed()
 
         if pressed_keys[pygame.K_UP]:
